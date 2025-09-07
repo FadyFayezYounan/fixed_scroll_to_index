197f8d002968d4863ac609512e0d3bde1a84ca16
FadyFayezYounan <fadyfayez067@gmail.com>
2025-09-07 19:30:48 +0300

    add exports for fixed_scroll_to_index_provider and fixed_scroll_to_index_builder

--- lib/fixed_scroll_to_index.dart
+++ lib/fixed_scroll_to_index.dart
@@ -1,2 +1,4 @@
 export 'src/controllers/fixed_scroll_to_index_controller.dart';
 export 'src/core/scrollable_config.dart';
+export 'src/providers/fixed_scroll_to_index_provider.dart';
+export 'src/widgets/fixed_scroll_to_index_builder.dart';
